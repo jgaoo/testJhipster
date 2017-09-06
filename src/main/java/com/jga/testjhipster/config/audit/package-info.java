8646f89799263c5569facc9478af73530748beec
Gao <Jie.Gao@eves-it.de>
2017-09-06 10:46:03 +0200

    test

---
+++ src/main/java/com/jga/testjhipster/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.jga.testjhipster.config.audit;
---
+++ src/main/java/com/jga/testjhipster/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.jga.testjhipster.repository;
+
+import com.jga.testjhipster.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
